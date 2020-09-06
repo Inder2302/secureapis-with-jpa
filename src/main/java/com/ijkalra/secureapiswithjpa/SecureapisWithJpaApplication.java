a23785e2aafc94479a69ec3e79d2bfc32dc05411
Indrajeet Kalra <Inder@Indrajeets-MacBook-Pro.local>
2020-09-06 16:10:19 +1000

    Initial commit

---
+++ src/main/java/com/ijkalra/secureapiswithjpa/SecureapisWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.ijkalra.secureapiswithjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureapisWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureapisWithJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ijkalra/secureapiswithjpa/SecureapisWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijkalra.secureapiswithjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureapisWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
